159ea720ed7d1c3720a5ee3e0a84e2f728b81272
Steven Kim <sskim14@gmail.com>
2019-12-10 01:52:44 -0500

    Commented out media queries in CSS file and prepared responsiveness without footer. Moved icons into navbar.

--- src/App.js
+++ src/App.js
@@ -18,51 +18,52 @@ class App extends Component {
   render() {
     return (
       <Router>
-        <nav className="navbar navbar-expand-lg navbar-light bg-info">
+        <nav className="navbar navbar-expand-lg navbar-light nav-background">
           <Link to="/" className="navbar-brand text-white">
             Home
           </Link>
-          <ul className="navbar-nav">
-            <Link to="/Projects" className="nav-item nav-link link text-white">
-              Projects
-            </Link>
-            <Link to="/Contact" className="nav-item nav-link link text-white">
-              Contacts
-            </Link>
-            <a
-              className="nav-item nav-link link text-white"
-              href={myResume}
-              target="__blank"
-              rel="noopener noreferrer"
-            >
-              Resume
-            </a>
-          </ul>
+          <div className="nav-container">
+            <ul className="navbar-nav link-container">
+              <Link to="/Projects" className="nav-item nav-link text-white">
+                Projects
+              </Link>
+              <Link to="/Contact" className="nav-item nav-link text-white">
+                Contacts
+              </Link>
+              <a
+                className="nav-item nav-link text-white"
+                href={myResume}
+                target="__blank"
+                rel="noopener noreferrer"
+              >
+                Resume
+              </a>
+            </ul>
+            <div className="icon-container">
+              <a
+                className="About-icon fa fa-medium"
+                href="https://medium.com/@bolaadebesin"
+                target="_blank"
+                rel="noopener noreferrer"
+              />
+              <a
+                className="About-icon fa fa-github-square"
+                target="_blank"
+                href="https://github.com/MobolanleAdebesin"
+                rel="noopener noreferrer"
+              />
+              <a
+                className="About-icon fa fa-linkedin"
+                target="_blank"
+                href="https://www.linkedin.com/in/mobolanle-adebesin/"
+                rel="noopener noreferrer"
+              />
+            </div>
+          </div>
         </nav>
         <Route path="/" exact component={About}></Route>
         <Route path="/Projects" exact component={Projects}></Route>
         <Route path="/Contact" exact component={Contact}></Route>
-
-        <footer className="App-footer">
-          <a
-            className="About-icon fa fa-medium"
-            href="https://medium.com/@bolaadebesin"
-            target="_blank"
-            rel="noopener noreferrer"
-          ></a>
-          <a
-            className="About-icon fa fa-github-square"
-            target="_blank"
-            href="https://github.com/MobolanleAdebesin"
-            rel="noopener noreferrer"
-          ></a>
-          <a
-            className="About-icon fa fa-linkedin"
-            target="_blank"
-            href="https://www.linkedin.com/in/mobolanle-adebesin/"
-            rel="noopener noreferrer"
-          ></a>
-        </footer>
       </Router>
     );
   }
