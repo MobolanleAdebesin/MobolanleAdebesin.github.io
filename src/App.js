d5e4ea9ebae146cf83e133dda06a3f87d6e9e119
Mobolanle Adebesin <bolaadebesin@gmail.com>
2019-12-10 20:47:04 -0500

    Set menu display to none. Created sliding menu that does not slide yet. OnClick event added to hamburger icon.

--- src/App.js
+++ src/App.js
@@ -11,59 +11,74 @@ class App extends Component {
     super(props);
     this.state = {
       icon: "fa fa-bars",
-      navbar: "navbar"
+      navbar: "navbar",
+      menu: "hidden"
     };
   }
+  slide = () => {
+    if (this.state.menu == "hidden") {
+      this.setState({ menu: "App-menu" });
+    } else if (this.state.menu == "App-menu") {
+      this.setState({ menu: "hidden" });
+    }
+  };
 
   render() {
     return (
       <Router>
-        <nav className="navbar navbar-expand-lg navbar-light nav-background">
-          <Link to="/" className="navbar-brand text-white">
-            Home
-          </Link>
-          <div className="nav-container">
-            <ul className="navbar-nav link-container">
-              <Link to="/Projects" className="nav-item nav-link text-white">
-                Projects
-              </Link>
-              <Link to="/Contact" className="nav-item nav-link text-white">
-                Contacts
-              </Link>
-              <a
-                className="nav-item nav-link text-white"
-                href={myResume}
-                target="__blank"
-                rel="noopener noreferrer"
-              >
-                Resume
-              </a>
-            </ul>
-            <div className="icon-container">
-              <a
-                className="About-icon fa fa-medium"
-                href="https://medium.com/@bolaadebesin"
-                target="_blank"
-                rel="noopener noreferrer"
-              />
-              <a
-                className="About-icon fa fa-github-square"
-                target="_blank"
-                href="https://github.com/MobolanleAdebesin"
-                rel="noopener noreferrer"
-              />
-              <a
-                className="About-icon fa fa-linkedin"
-                target="_blank"
-                href="https://www.linkedin.com/in/mobolanle-adebesin/"
-                rel="noopener noreferrer"
-              />
-            </div>
+        <div className={this.state.menu}></div>
+        <div className="App-container">
+          <div className="App-hamburger">
+            <i class="fa fa-bars" onClick={this.slide}></i>
           </div>
-        </nav>
-        <Route path="/" exact component={About}></Route>
-        <Route path="/Projects" exact component={Projects}></Route>
-        <Route path="/Contact" exact component={Contact}></Route>
+
+          <nav className="navbar navbar-expand-lg navbar-light nav-background">
+            <Link to="/" className="navbar-brand text-white">
+              Home
+            </Link>
+            <div className="nav-container">
+              <ul className="navbar-nav link-container">
+                <Link to="/Projects" className="nav-item nav-link text-white">
+                  Projects
+                </Link>
+                <Link to="/Contact" className="nav-item nav-link text-white">
+                  Contacts
+                </Link>
+                <a
+                  className="nav-item nav-link text-white"
+                  href={myResume}
+                  target="__blank"
+                  rel="noopener noreferrer"
+                >
+                  Resume
+                </a>
+              </ul>
+              <div className="icon-container">
+                <a
+                  className="About-icon fa fa-medium"
+                  href="https://medium.com/@bolaadebesin"
+                  target="_blank"
+                  rel="noopener noreferrer"
+                />
+                <a
+                  className="About-icon fa fa-github-square"
+                  target="_blank"
+                  href="https://github.com/MobolanleAdebesin"
+                  rel="noopener noreferrer"
+                />
+                <a
+                  className="About-icon fa fa-linkedin"
+                  target="_blank"
+                  href="https://www.linkedin.com/in/mobolanle-adebesin/"
+                  rel="noopener noreferrer"
+                />
+              </div>
+            </div>
+          </nav>
+          <Route path="/" exact component={About}></Route>
+          <Route path="/Projects" exact component={Projects}></Route>
+          <Route path="/Contact" exact component={Contact}></Route>
+        </div>
       </Router>
     );
   }
