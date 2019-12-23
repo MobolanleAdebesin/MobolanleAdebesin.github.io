dc33f268e723f1917e5e68eaca27afa42751e629
Mobolanle Adebesin <bolaadebesin@gmail.com>
2019-12-23 14:17:46 -0500

    Removed screenshots of previous projects. Added new images to image folder. Created JSON file for project info

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -1,13 +1,10 @@
 import React, { Component } from "react";
 import "./Projects.css";
-// import Trivia from "./images/Trivia.png";
-// import Bookpals from "./images/Books.png";
-// import Tinder from "./images/Tinder.png";
 class Projects extends Component {
   render() {
     return (
-      <div>
-        <h1>This is where the projects go!</h1>
+      <div className="Projects">
+        <h1>Projects </h1>
       </div>
     );
   }
