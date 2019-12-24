5a1542b883a7bb3708b6cf569d7eb6d0cd650501
Mobolanle Adebesin <bolaadebesin@gmail.com>
2019-12-24 10:37:06 -0500

    Deleted media query  breakpoint at 1024px. Added padding to the About page. Removed unecessary divs from About page. Corrected styling in Contect and Projects pages.

--- src/components/About.js
+++ src/components/About.js
@@ -5,19 +5,23 @@ class About extends Component {
   render() {
     return (
       <main className="About-main-container">
-        <div className="About-name-container">
-          <h2 className="About-name">Bola Adebesin</h2>
+        <h2 className="About-name">Bola Adebesin</h2>
+        <div className="About-profile-title-description">
+          <div className="About-profile-title">
+            <img className="About-profile" src={profile} alt="profile" />
+            <h5 className="About-title">Software Engineer</h5>
+          </div>
+          <div className="About-description-container">
+            <p className="About-description">
+              While my approach to programming is
+              <strong> [analytical]</strong> and
+              <strong> [methodical] </strong>, my passion for technology and
+              discovery drives me to relentlessly pursue
+              <strong> [creative] </strong> solutions to complex problems.
+            </p>
+          </div>
         </div>
-        <img className="About-profile" src={profile} alt="profile" />
 
-        <h5 className="About-title">Software Engineer</h5>
-        <p className="About-description">
-          While my approach to programming is
-          <strong> [analytical]</strong> and
-          <strong> [methodical] </strong>, my passion for technology and
-          discovery drives me to relentlessly pursue
-          <strong> [creative] </strong> solutions to complex problems.
-        </p>
         <div className="About-links">
           <a
             href="https://github.com/MobolanleAdebesin"
