6d1a768868d97d8f671709fba0050c9485284ae2
Mobolanle Adebesin <bolaadebesin@gmail.com>
2019-12-24 08:25:00 -0500

    Formatted project div, included fontawesome links to repo and deployed site, added styling to project text

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -1,10 +1,51 @@
 import React, { Component } from "react";
 import "./Projects.css";
+import projects from "../data/projects.js";
+
 class Projects extends Component {
+  constructor(props) {
+    super(props);
+  }
   render() {
     return (
       <div className="Projects">
         <h1>Projects </h1>
+        {projects.map(project => (
+          <div key={project.name} className="Projects-card">
+            <img
+              className="Project-image"
+              src={project.image}
+              alt="project-image"
+            />
+            <div className="Project-overlay"></div>
+            <div className="Project-info-container">
+              <h3 className="Project-name">{project.name}</h3>
+
+              {project.link.Repo ? (
+                <a
+                  href={project.link.Repo}
+                  target="_blank"
+                  rel="noopener noreferrer"
+                  className="Project-link"
+                >
+                  <i className="fa fa-github-square fa-2x Project-fa"></i>
+                </a>
+              ) : null}
+
+              {project.link.Deployed ? (
+                <a
+                  href={project.link.Deployed}
+                  target="_blank"
+                  rel="noopener noreferrer"
+                  className="Project-link"
+                >
+                  <i className="fa fa-link fa-2x Project-fa"></i>
+                </a>
+              ) : null}
+              <p className="Project-info">{project.description}</p>
+            </div>
+          </div>
+        ))}
       </div>
     );
   }
---
+++ src/data/projects.js
@@ -0,0 +1,96 @@
+export default [
+  {
+    name: "Bookpals",
+    image: require("../components/images/BookPals-SS.png"),
+    description:
+      "A full-stack application with CRUD functionality. Allows users to share info about their favorite books.",
+    stack: [
+      "HTML",
+      " CSS",
+      " JavaScript",
+      " React",
+      " MongoDB",
+      " Express",
+      " Node"
+    ],
+    link: {
+      Repo: "https://github.com/MobolanleAdebesin/bookpals-front-end",
+      Deployed: "https://stupefied-knuth-a58fed.netlify.com/"
+    }
+  },
+  {
+    name: "Bookmark Collector",
+    image: require("../components/images/Bookmark-SS.png"),
+    description:
+      "A Python-PostgreSQL command line application that allows users to store data about their favorite websites. Includes a GUI built with Tkinter",
+    stack: ["Python", " PeeWee", " PostgreSQL", " Tkinter"],
+    link: {
+      Repo: "https://github.com/MobolanleAdebesin/bookmark-collector",
+      Deployed: false
+    }
+  },
+  {
+    name: "Fairytale Trivia",
+    image: require("../components/images/Trivia-SS.png"),
+    description:
+      "An application that uses DOM manipulation to allow users to test their knowledge about popular fairytales.",
+    stack: ["HTML", " CSS", " JavaScript"],
+    link: {
+      Repo: "https://github.com/MobolanleAdebesin/trivia-game",
+      Deployed: "https://mobolanleadebesin.github.io/trivia-game/"
+    }
+  },
+  {
+    name: "CoTripper",
+    image: require("../components/images/CoTrip-SS.png"),
+    description:
+      "An open-source client project designed to facilitate group travel experiences for single mothers.",
+    stack: [
+      "Python",
+      " Django",
+      " JavaScript",
+      " React",
+      " HTML",
+      " CSS",
+      " Storybook"
+    ],
+    link: {
+      Repo: "https://github.com/CotripperPlatform/CoTrip",
+      Deployed: false
+    }
+  },
+
+  {
+    name: "Component Library ",
+    image: require("../components/images/Library-SS.png"),
+    description:
+      "A React component library comprised of modular, reusable components for various websites.",
+    stack: ["HTML", " CSS", " JavaScript", " React", " Storybook"],
+    link: {
+      Repo: "https://github.com/MobolanleAdebesin/component-library",
+      Deployed:
+        "https://agitated-austin-92807a.netlify.com/?path=/story/button--primary"
+    }
+  },
+  {
+    name: "Simon",
+    image: require("../components/images/Simon-SS.png"),
+    description:
+      "An application that allows users to test their memory, based on the popular Simon game.",
+    stack: ["HTML", " CSS", " JavaScript", " jQuery"],
+    link: {
+      Repo: "https://github.com/MobolanleAdebesin/Simon-Says-Game",
+      Deployed: "https://mobolanleadebesin.github.io/Simon-Says-Game/"
+    }
+  },
+  {
+    name: "Tinder Mock",
+    image: require("../components/images/Tinder-SS.png"),
+    description: "A website mock up based on a popular dating application",
+    stack: ["HTML", " CSS"],
+    link: {
+      Repo: "https://github.com/MobolanleAdebesin/mock-tinder-site",
+      Deployed: "https://mobolanleadebesin.github.io/mock-tinder-site/"
+    }
+  }
+];
